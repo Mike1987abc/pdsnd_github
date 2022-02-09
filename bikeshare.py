d3acce39bf9fc92e6a9a06cf942a726b06241bcb
Michael Festner <michael.festner@bmw.de>
2022-02-09 19:44:53 +0100

    Change string for City input

--- bikeshare.py
+++ bikeshare.py
@@ -19,10 +19,10 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    city = input('What city your are interested in?\nType either:\nc for chicago, nyc for New York City or w for washington\n')
+    city = input('What city your are interested in?\nType either:\nc for Chicago, nyc for New York City or w for Washington\n')
     city = city.lower()
     while city not in ['c', 'nyc', 'w']:
-        city = input('Incorrect input!\n Type either:\nc for chicago, nyc for New York City or w for washington\n')
+        city = input('Incorrect input!\n Type either:\nc for Chicago, nyc for New York City or w for Washington\n')
         city = city.lower()
 
     # TO DO: get user input for month (all, january, february, ... , june)
